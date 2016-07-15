5f9cad2a191d914bf6bb78e641a3d65a3184041e
Tim Channell <tim.channell@gmail.com>
2016-07-15 13:07:53 -0400

    initial commit

---
+++ kinesis-reader.js
@@ -0,0 +1,142 @@
+var from2 = require('from2');
+var Promise = require('promise');
+var xtend = require('xtend');
+
+module.exports = function (kinesis, streamName, iteratorType, opts) {
+  var streamState = initStreamState(kinesis, streamState, iteratorType, opts);
+
+  var readable = from2.obj(function (size, next) {
+    if (closed) return next(null, null);
+    var promiseChain;
+
+    var promiseChain = (streamState.shards)
+      ? getRecords(streamState)
+      : getShardList(streamState).then(cycleShards).then(getRecords);
+
+    promiseChain
+      .catch(catchCycle(streamState))
+      .nodeify(next);
+  });
+
+  readable.close = function () {closed = true;};
+  readable.streamState = streamState;
+  return readable;
+};
+
+function catchCycle(streamState) {
+  return function (error) {
+    return new Promise(function (resolve, reject) {
+      // right now, we'll only handle provisioned throughputs errors. Otherwise, we'll emit an error
+      if (!error.code || error.code !== 'ProvisionedThroughputExceededException') return reject(error);
+      
+      return cycleShards(streamState)
+        .then(function () { resolve([]); })
+    });
+  };
+}
+
+function cycleShards(streamState) {
+  return new Promise(function (resolve, reject) {
+    streamState.currShard++;
+    
+    if (streamState.currShard >= streamState.shards.length) {
+      if (!streamState.options.allowLooping) return reject('all shards exhausted');
+      streamState.currShard = 0;
+    }
+
+    setTimeout(function () {
+      resolve(streamState);
+    }, streamState.options.cyclepause);
+  }).then(getShardIterator);
+}
+
+function getRecords(streamState) {
+  var shard = streamState.shards[streamState.currShard];
+  var iter = streamState.shardIterators[shard];
+
+  return new Promise(function (resolve, reject) {
+    streamState.kinesis.getRecords({ShardIterator: iter}, function (err, data) {
+      if (err) return reject(err);
+
+      // Update iterator
+      streamState.shardIterators[shard] = data.NextShardIterator;
+      setTimeout(function () {
+        resolve(data.Records);
+      }, streamState.options.readpause);
+    });
+  });
+}
+
+function getShardList(streamState, startShard) {
+  if (!streamState.shards) streamState.shards = [];
+
+  var options = {StreamName: streamState.streamName};
+  if (startShard) options.ExclusiveStartShardId = startShard;
+
+  return new Promise(function (resolve, reject) {
+    streamState.kinesis.describeStream(options, function (err, data) {
+      if (err) return reject(err);
+
+      data.StreamDescription.Shards.forEach(function (s) {
+        streamState.shards.push(s.ShardId);
+      });
+
+      if (data.StreamDescription.HasMoreShards) {
+        // ask for more shards, starting from the last one we got
+        getShardList(streamState, streamState.shards[streamState.shards.length - 1])
+          .catch(reject)
+          .then(resolve);
+      } else {
+        resolve(streamState);
+      }
+    });
+  });
+}
+
+function getShardIterator(streamState) {
+  var shard = streamState.shards[streamState.currShard];
+
+  return new Promise(function (resolve, reject) {
+    streamState.kinesis.getShardIterator({
+      StreamName: streamState.streamName,
+      ShardId: shard,
+      ShardIteratorType: streamState.iteratorType
+    }, function (e, d) {
+      if (e) return reject(e);
+
+      streamState.shardIterators[shard] = d.ShardIterator;
+      return resolve(streamState);
+    });
+  });
+}
+
+function initStreamState(kinesis, streamName, iteratorType, opts) {
+  if (!options) options = {};
+
+  var options = xtend({
+    readpause: 1000,
+    cyclepause: 1000,
+    allowLooping: false
+  }, opts);
+
+  var closed = false;
+  var streamState = {
+    streamName: streamName,
+    shards: null,
+    currShard: -1,
+    shardIterators: {},
+    iteratorType: iteratorType,
+    kinesis: kinesis,
+    options: options
+  };
+
+  return streamState;
+}
+
+module.exports.catchCycle = catchCycle;
+module.exports.cycleShards = cycleShards;
+module.exports.getShardIterator = getShardIterator;
+module.exports.getShardList = getShardList;
+module.exports.getRecords = getRecords;
+module.exports.cyleShards = cycleShards;
+module.exports.initStreamState = initStreamState;
---
+++ test/kinesis-reader.test.js
@@ -0,0 +1,293 @@
+var test = require('tap').test,
+  reader = require('../'),
+  AWS = require('aws-sdk'),
+  AWSMock = require('aws-sdk-mock');
+
+
+test('catchCycle retries on throughput exceptions', function (t) {
+  AWSMock.mock('Kinesis', 'getShardIterator', function (params, cb) {
+    cb(null, {ShardIterator: 'ABCD'});
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+  streamState.shards = ['shard-1', 'shard-2'];
+
+  reader.catchCycle(streamState)({code: 'ProvisionedThroughputExceededException'}).nodeify(function (err, result) {
+    t.error(err);
+    t.equal(streamState.shardIterators['shard-1'], 'ABCD', 'shard iterator was updated');
+    AWSMock.restore('Kinesis');
+    t.end();
+  });
+});
+
+test('catchCycle fails on other errors', function (t) {
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+  streamState.shards = ['shard-1'];
+
+  reader.catchCycle(streamState)({code: 'AnotherException'}).nodeify(function (err, result) {
+    t.equal(err.code, 'AnotherException', 'catchCycle rejected with the expected error');
+    t.end();
+  });
+});
+
+test('cycleShards moves to the next shard', function (t) {
+  AWSMock.mock('Kinesis', 'getShardIterator', function (params, cb) {
+    cb(null, {ShardIterator: 'ABCD'});
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+  streamState.shards = ['shard-1', 'shard-2'];
+
+  var succeeded = 0;
+
+  function afterCycle(expected) {
+    return function () {
+      succeeded++;
+      t.equal(expected, streamState.currShard, 'cycled to ' + expected + ' shard');
+      return streamState;
+    }
+  }
+
+  reader.cycleShards(streamState) // Initialize onto the first shard
+    .then(afterCycle(0))
+    .then(reader.cycleShards) // Move to the second shard
+    .then(afterCycle(1))
+    .then(function () {
+      t.equal(succeeded, 2);
+      AWSMock.restore('Kinesis');
+      t.end();
+    });
+});
+
+test('cycleShards loops around if allowLooping = true', function (t) {
+  AWSMock.mock('Kinesis', 'getShardIterator', function (params, cb) {
+    cb(null, {ShardIterator: 'ABCD'});
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {allowLooping: true});
+  streamState.shards = ['shard-1', 'shard-2'];
+
+  var succeeded = 0;
+
+  function afterCycle() {
+    succeeded++;
+    return streamState;
+  }
+
+  reader.cycleShards(streamState) // Initialize onto the first shard
+    .then(afterCycle)
+    .then(reader.cycleShards) // Move to the second shard
+    .then(afterCycle)
+    .then(reader.cycleShards) // Loop back to the first
+    .then(afterCycle)
+    .then(function () {
+      t.equal(streamState.currShard, 0, 'looped back to the first shard');
+      t.equal(succeeded, 3, 'confirm that we cycled 3 times');
+      AWSMock.restore('Kinesis');
+      t.end();
+    });
+});
+
+test('cycleShards fails if looping is disabled an no more shards exist', function (t) {
+  AWSMock.mock('Kinesis', 'getShardIterator', function (params, cb) {
+    cb(null, {ShardIterator: 'ABCD'});
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+  streamState.shards = ['shard-1', 'shard-2'];
+
+  var succeeded = 0;
+
+  function afterCycle() {
+    succeeded++;
+    return streamState;
+  }
+
+  reader.cycleShards(streamState)
+    .then(afterCycle)
+    .then(reader.cycleShards)
+    .then(afterCycle)
+    .then(reader.cycleShards)
+    .then(afterCycle)
+    .nodeify(function (err, res) {
+      t.ok(err, 'received error after cycling past the end of the shard list');
+      t.equal(succeeded, 2, 'two successful cycles before failing');
+      t.end();
+    });
+});
+
+test('getRecords resolves with fetched kinesis records', function (t) {
+  AWSMock.mock('Kinesis', 'getRecords', function (params, cb) {
+    t.equal(params.ShardIterator, 'ABCD');
+    cb(null, {
+      NextShardIterator: 'EFGH',
+      Records: [
+        {id: 1},
+        {id: 2}
+      ]
+    });
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+  streamState.shards = ['shard-1', 'shard-2'];
+  streamState.currShard = 0;
+  streamState.shardIterators = {'shard-1': 'ABCD'};
+
+  reader.getRecords(streamState).nodeify(function (err, res) {
+    t.error(err, 'no error');
+    t.deepEqual(res, [{id: 1}, {id: 2}], 'resolved with expected results');
+    t.equal(streamState.shardIterators['shard-1'], 'EFGH', 'shard iterator was updated')
+    AWSMock.restore('Kinesis');
+    t.end();
+  });
+});
+
+test('getRecords rejects with kinesis errors', function (t) {
+  AWSMock.mock('Kinesis', 'getRecords', function (params, cb) {
+    t.equal(params.ShardIterator, 'ABCD');
+    cb('An error occurred');
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+  streamState.shards = ['shard-1', 'shard-2'];
+  streamState.currShard = 0;
+  streamState.shardIterators = {'shard-1': 'ABCD'};
+
+  reader.getRecords(streamState).nodeify(function (err) {
+    t.equal(err, 'An error occurred', 'rejected with the expected error');
+    AWSMock.restore('Kinesis');
+    t.end();
+  });
+});
+
+
+test('getShardList sets list of shards for streamState', function (t) {
+  AWSMock.mock('Kinesis', 'describeStream', function (params, cb) {
+    cb(null, {
+      StreamDescription: {
+        Shards: [
+          {ShardId: 'shard-0'},
+          {ShardId: 'shard-1'},
+          {ShardId: 'shard-2'},
+        ],
+        HasMoreShards: false
+      }
+    });
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+
+  reader.getShardList(streamState).nodeify(function (err, res) {
+    t.deepEqual(streamState.shards, [
+      'shard-0', 
+      'shard-1', 
+      'shard-2'
+    ], 'streamState.shards matches expected');
+    AWSMock.restore('Kinesis');
+    t.end();
+  });
+});
+
+test('getShardList loops when there HasMoreShards is true', function (t) {
+  var called = 0;
+  AWSMock.mock('Kinesis', 'describeStream', function (params, cb) {
+    called++;
+    if (called == 1) {
+      cb(null, {
+        StreamDescription: {
+          Shards: [
+            {ShardId: 'shard-0'},
+            {ShardId: 'shard-1'},
+            {ShardId: 'shard-2'},
+          ],
+          HasMoreShards: true
+        }
+      });
+    } else {
+      cb(null, {
+        StreamDescription: {
+          Shards: [
+            {ShardId: 'shard-3'},
+            {ShardId: 'shard-4'},
+            {ShardId: 'shard-5'},
+          ],
+          HasMoreShards: false
+        }
+      }); 
+    }
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+
+  reader.getShardList(streamState).nodeify(function (err, res) {
+    t.deepEqual(streamState.shards, [
+      'shard-0', 
+      'shard-1', 
+      'shard-2', 
+      'shard-3', 
+      'shard-4', 
+      'shard-5'
+    ], 'streamState.shards matches expected');
+    AWSMock.restore('Kinesis');
+    t.end();
+  });
+});
+
+test('getShardList rejects with kinesis errors', function (t) {
+  AWSMock.mock('Kinesis', 'describeStream', function (params, cb) {
+    cb('An error occurred');
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+
+  reader.getShardList(streamState).nodeify(function (err, res) {
+    t.equal(err, 'An error occurred');
+    AWSMock.restore('Kinesis');
+    t.end();
+  });
+});
+
+test('getShardIterator sets streamState.shardIterators', function (t) {
+  AWSMock.mock('Kinesis', 'getShardIterator', function (params, cb) {
+    cb(null, {ShardIterator: 'ABCD'});
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+  streamState.shards = ['shard-1'];
+  streamState.currShard = 0;
+
+  reader.getShardIterator(streamState).nodeify(function (err, res) {
+    t.error(err);
+    t.deepEqual(streamState.shardIterators, {'shard-1': 'ABCD'});
+    AWSMock.restore('Kinesis');
+    t.end();
+  });
+});
+
+test('getShardIterator rejects with kinesis errors', function (t) {
+  AWSMock.mock('Kinesis', 'getShardIterator', function (params, cb) {
+    cb('An error occurred');
+  });
+
+  var kinesis = new AWS.Kinesis();
+  var streamState = reader.initStreamState(kinesis, 'test-stream', 'TRIM_HORIZON', {});
+  streamState.shards = ['shard-1'];
+  streamState.currShard = 0;
+
+  reader.getShardIterator(streamState).nodeify(function (err, res) {
+    t.equal(err, 'An error occurred');
+    AWSMock.restore('Kinesis');
+    t.end();
+  });
+});
